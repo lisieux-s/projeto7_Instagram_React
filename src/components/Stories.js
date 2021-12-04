70f420cb3e6e38b429ed0812a7f7162280f421ec
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2021-12-04 18:19:48 -0300

    using map but not using story.js on map

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,10 +1,37 @@
 import Story from "./Story";
 export default function Stories() {
   //array de stories
-  const stories = []
+  const stories = [
+    { imgURL: "assets/img/9gag.svg", user: "9gag" },
+    { imgURL: "assets/img/meowed.svg", user: "meowed" },
+    { imgURL: "assets/img/barked.svg", user: "barked" },
+    { imgURL: "assets/img/nathanwpylestrangeplanet.svg", user: "nathanwpylestrangeplanet" },
+    { imgURL: "assets/img/wawawicomics.svg", user: "wawawicomics" },
+    { imgURL: "assets/img/respondeai.svg", user: "respondeai" },
+    { imgURL: "assets/img/filomoderna.svg", user: "filomoderna" },
+    { imgURL: "assets/img/memeriagourmet.svg", user: "memeriagourmet" },
+  ];
   return (
     //ok now do it using map
     <div class="stories">
+      {stories.map((story) => (
+        <div class="story">
+          <div class="imagem">
+            <img src={story.imgURL} />
+          </div>
+          <div class="usuario">{story.user}</div>
+        </div>
+      ))}
+      <div class="setinha">
+        <ion-icon name="chevron-forward-circle"></ion-icon>
+      </div>
+    </div>
+  );
+        
+}
+
+/*
+ <div class="stories">
       <Story imgURL="assets/img/9gag.svg" user="9gag" />
       <Story imgURL="assets/img/meowed.svg" user="meowed" />
       <Story imgURL="assets/img/barked.svg" user="barked" />
@@ -13,12 +40,10 @@ export default function Stories() {
       <Story imgURL="assets/img/respondeai.svg" user="respondeai" />
       <Story imgURL="assets/img/filomoderna.svg" user="filomoderna" />
       <Story imgURL="assets/img/memeriagourmet.svg" user="memeriagourmet" />
-      
-      <div class="setinha">
+
+            <div class="setinha">
         <ion-icon name="chevron-forward-circle"></ion-icon>
       </div>
     </div>
-    
-    
-  );
-}
+
+*/
