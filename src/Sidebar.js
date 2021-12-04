529516f6f2249564f63806953a63b6060395af2d
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2021-12-04 16:50:35 -0300

    componentization (broader strokes)

---
+++ src/Sidebar.js
@@ -0,0 +1,26 @@
+import Sugestoes from "./Sugestoes"
+export default function Sidebar() {
+    return (
+        //sugestao é elemento repetitivo, então sugiro props
+        <div class="sidebar">
+          <div class="usuario">
+            <img src="assets/img/catanacomics.svg" />
+            <div class="texto">
+              <strong>catanacomics</strong>
+              Catana
+            </div>
+          </div>
+        
+        <Sugestoes />
+
+          <div class="links">
+            Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
+          </div>
+
+          <div class="copyright">
+            © 2021 INSTAGRAM DO FACEBOOK
+          </div>
+        </div>
+    )
+}
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 //lets start with the header. how hard can it be?
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import Navbar from './Navbar';
+import Corpo from './Corpo';
+import Rodape from './Rodape';
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Corpo />
+            <Rodape />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"))
