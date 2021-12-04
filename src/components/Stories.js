b1e8e2e1995856214e977d7bbb2b1673f3e3afbc
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2021-12-04 18:32:38 -0300

    stories done

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,6 +1,5 @@
 import Story from "./Story";
 export default function Stories() {
-  //array de stories
   const stories = [
     { imgURL: "assets/img/9gag.svg", user: "9gag" },
     { imgURL: "assets/img/meowed.svg", user: "meowed" },
@@ -12,7 +11,6 @@ export default function Stories() {
     { imgURL: "assets/img/memeriagourmet.svg", user: "memeriagourmet" },
   ];
   return (
-    //gente, to emocionada, ta dando certo??
     <div class="stories">
       {stories.map((story) => (Story(story)
       ))}
@@ -23,21 +21,3 @@ export default function Stories() {
   );
         
 }
-
-/*
- <div class="stories">
-      <Story imgURL="assets/img/9gag.svg" user="9gag" />
-      <Story imgURL="assets/img/meowed.svg" user="meowed" />
-      <Story imgURL="assets/img/barked.svg" user="barked" />
-      <Story imgURL="assets/img/nathanwpylestrangeplanet.svg" user="nathanwpylestrangeplanet" />
-      <Story imgURL="assets/img/wawawicomics.svg" user="wawawicomics" />
-      <Story imgURL="assets/img/respondeai.svg" user="respondeai" />
-      <Story imgURL="assets/img/filomoderna.svg" user="filomoderna" />
-      <Story imgURL="assets/img/memeriagourmet.svg" user="memeriagourmet" />
-
-            <div class="setinha">
-        <ion-icon name="chevron-forward-circle"></ion-icon>
-      </div>
-    </div>
-
-*/
